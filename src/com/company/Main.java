b2ea8e45814dfe01cede0f55ac144546e88cd8ed
Thomas H O'Hara <to007@pvhs-lab112-d18.pvsd.org>
2016-09-08 14:29:57 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Declare variable you:
+        //String or int or double
+        String s;
+        s = "name";//initialize
+        String s2 = "nickname";
+        System.out.println(s);
+        /*Naming rules of variables
+            1. if, else, void, public
+            Avoid all reserved words.
+            2. Can't start with a number
+            3. No special characters
+            4. No Spaces
+            5. reuse a variable name
+            String vs int
+            int vs double
+         */
+        int a = 45;
+        double b = 3;
+        double sum = a + b;
+
+        a = 2_342_342;
+        int E,d = 0;
+        String samthing = "";
+    }
+}
